7d15453755ad0ec7443e9ad6fb43c5864910a98c
Margot Konig <margot.konig@blackrock.com>
2019-05-25 17:25:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.find_all {|w| w.split("").sort == @word.split("").sort}
+  
+ end
+end
+
