67f84d024ead7575d3dcf6fc560a0ce1be5e3638
Margot Konig <margot.konig@blackrock.com>
2019-05-28 16:30:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,13 +4,16 @@ require "pry"
 class Anagram
   attr_accessor :word 
   
+  def word 
+    @word 
+  end
+  
   def initialize(word)
     @word = word
   end
   
   def match(words)
     words.find_all {|w| w.split("").sort == @word.split("").sort}
-  
  end
 end
 
